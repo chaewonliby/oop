46454b4d659c026c78f29815ad9c71584494c43c
chaewonliby <ichaewoni01@gmail.com>
2025-09-11 18:35:04 +0900

    Homework1

---
+++ week2/Homework1/Homework1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Homework1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        int[] student = new int[5];
+
+        for (int num = 0; num < 5; num++) {
+            System.out.print("정수를 입력하세요 (" + (num + 1) + "번째): ");
+            student[num] = sc.nextInt();
+            sum += student[num];
+        }
+        System.out.printf("현재까지 입력된 정수의 합은 %d입니다. " , sum);
+    }
+}
